1b10e22bf188a4a121f97bcd9fb98a693548ddd5
ramus-ohe <rassemichael@gmail.com>
2026-09-07 13:30:40 +0300

    hyprmoncfg added

--- install_scripts/scripts/displays.sh
+++ install_scripts/scripts/displays.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 packages=(
-  nwg-displays  # Display manager
+  hyprmoncfg-bin  # Monitor configuration
   brightnessctl  # Brightness control
 )
 
 # Install packages
 yay -S --noconfirm --needed "${packages[@]}"
+
+# Enable services
+systemctl --user enable --now hyprmoncfg.service
